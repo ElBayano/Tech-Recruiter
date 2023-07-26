3bbd3448e7627034d782d363796e25e313affa70
ElBayano <samdeveloperbr@gmail.com>
2023-07-26 16:54:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/inputs/inputLog.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "/src/assets/icons/emailIcon.svg"
+import "./styles"
+import { LoginIcon, Input, ContainerInput, } from "./styles";
+
+export default function InputLog({type, placeholder}) {
+
+    const email = "input-log-icon-email"
+    const password = "input-log-icon-password"
+    const srcImg = type === "password" ? "passwordIcon" : "emailIcon"
+    const typeOfClass = type === "password"? password : email
+    
+return (
+    <ContainerInput>
+        <LoginIcon src={`/src/assets/icons/${srcImg}.svg`} alt="icone de email" className={typeOfClass}/>
+        <Input type={type} className="input-log" placeholder={placeholder}></Input>
+    </ContainerInput>
+)}
